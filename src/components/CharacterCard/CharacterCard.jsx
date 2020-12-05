c9191685fe9fd90bdec27f8b8308a28f5e7d3185
Jennifer Goijman <jennifer.goijman@bourne-leisure.co.uk>
2020-12-05 22:21:43 +0100

    add like

--- src/components/CharacterCard/CharacterCard.jsx
+++ src/components/CharacterCard/CharacterCard.jsx
@@ -1,14 +1,27 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import ThemeContext from "../../context/ThemeContext";
 import "./CharacterCard.css";
 
 const CharacterCard = ({ character }) => {
   const { darkTheme } = useContext(ThemeContext);
+  const [isLiked, setIsLiked] = useState(false);
+
+  const handleLike = () => {
+    setIsLiked(!isLiked);
+  };
 
   return (
     <div className={`CharacterCard ${darkTheme ? "dark" : "light"}`}>
-      <h2 key={character.id}>{character.name}</h2>
-      <img src={character.image} alt=""></img>
+      <div className="Header">
+        <h2 key={character.id}>{character.name}</h2>
+        <img
+          src={`/img/icon-heart-${isLiked ? "full" : "empty"}.png`}
+          alt=""
+          className="Icon"
+          onClick={handleLike}
+        />
+      </div>
+      <img src={character.image} alt="" className="Image" />
     </div>
   );
 };
