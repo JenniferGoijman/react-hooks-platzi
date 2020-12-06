dfd4cb8cbdc48cf3f3ad58f886a722d814f6937e
Jennifer Goijman <jennifer.goijman@bourne-leisure.co.uk>
2020-12-06 21:46:25 +0100

    add styles to Favorites

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -1,11 +1,22 @@
 import React from "react";
+import "./Favorites.css";
 
 const Favorites = ({ favorites }) => {
+  const lengthFavorites = favorites.favorites.length;
   return (
     <div className="Favorites">
-      {favorites.favorites.map((favorite) => (
-        <li key={favorite.id}>{favorite.name}</li>
-      ))}
+      {!!lengthFavorites && <h3>Favorites:</h3>}
+      <div className="Names">
+        {favorites.favorites.map((favorite, index) => (
+          <span key={favorite.id}>
+            {lengthFavorites !== 1 && index === lengthFavorites - 1 && (
+              <span> & </span>
+            )}
+            <span> {favorite.name}</span>
+            {index < lengthFavorites - 2 && <span>, </span>}
+          </span>
+        ))}
+      </div>
     </div>
   );
 };
